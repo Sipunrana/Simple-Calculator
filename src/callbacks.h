95340a76a30fd17ff8dffbf9396450d0a4e28324
Sipunrana <sipunrana6277@gmail.com>
2022-09-26 15:58:58 +0530

    simple-calculater

---
+++ src/callbacks.h
@@ -0,0 +1,82 @@
+#include <gtk/gtk.h>
+
+
+void
+on_backspace_clicked                   (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Clearentry_clicked                  (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_clear_clicked                       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_sign_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_division_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_nine_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_eight_clicked                       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_seven_clicked                       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_four_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_five_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_six_clicked                         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_mul_clicked                         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_subtrac_clicked                     (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_three_clicked                       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_two_clicked                         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_one_clicked                         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_zero_clicked                        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_dot_clicked                         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_equal_clicked                       (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_addition_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
